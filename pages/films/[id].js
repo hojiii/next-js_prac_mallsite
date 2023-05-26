e9140df75862bd1b0daec11df264886b81b4abc3
hojiii <shimn3060@naver.com>
2023-05-26 18:30:52 +0900

    fix:정적 생성 적용 영화리스트, 메인페이지

---
+++ pages/films/[id].js
@@ -0,0 +1,134 @@
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import MovieReviewList from '@/components/MovieReviewList';
+import Spinner from '@/components/Spinner';
+import styles from '@/styles/Movie.module.css';
+import axios from '@/lib/axios';
+import starImg from '@/public/star-filled.svg';
+
+const labels = {
+  rating: {
+    12: '12세이상관람가',
+    15: '15세이상관람가',
+    19: '청소년관람불가',
+    all: '전체관람가',
+  },
+};
+
+export async function getStaticPaths() {
+  const res = await axios.get('/movies/');
+  const movies = res.data.results ?? [];
+  const paths = movies.map((movie) => ({
+    params: { id: String(movie.id) },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params['id'];
+
+  try {
+    const res = await axios.get(`/movies/${id}`);
+    const movie = res.data;
+
+    return {
+      props: {
+        movie,
+      },
+    };
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
+}
+
+export default function Movie({ movie }) {
+  const [movieReviews, setMovieReviews] = useState([]);
+  const router = useRouter();
+  const id = router.query['id'];
+
+  async function loadMovieReviews(targetId) {
+    const res = await axios.get(`/movie_reviews/?movie_id=${targetId}`);
+    const nextMovieReviews = res.data.results ?? [];
+    setMovieReviews(nextMovieReviews);
+  }
+
+  useEffect(() => {
+    if (id) {
+      loadMovieReviews(id);
+    }
+  }, [id]);
+
+  if (!movie) {
+    return (
+      <div className={styles.loading}>
+        <Spinner />
+        <p>로딩중입니다. 잠시만 기다려주세요.</p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{movie.title} - watchit</title>
+      </Head>
+      <div className={styles.header}>
+        <div className={styles.posterContainer}>
+          <Image fill src={movie.posterUrl} alt={movie.name} />
+        </div>
+        <div className={styles.info}>
+          <div className={styles.englishTitle}>{movie.englishTitle}</div>
+          <h1 className={styles.title}>{movie.title}</h1>
+          <table className={styles.infoTable}>
+            <tbody>
+              <tr>
+                <th>개봉</th> <td>{movie.date}</td>
+              </tr>
+              <tr>
+                <th>장르</th> <td>{movie.genre}</td>
+              </tr>
+              <tr>
+                <th>국가</th> <td>{movie.country}</td>
+              </tr>
+              <tr>
+                <th>등급</th> <td>{labels.rating[movie.rating]}</td>
+              </tr>
+              <tr>
+                <th>러닝타임</th> <td>{movie.runningTime}분</td>
+              </tr>
+              <tr>
+                <th>평점</th>{' '}
+                <td className={styles.starRating}>
+                  <Image src={starImg} alt="★" />
+                  {movie.starRating}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <section className={styles.section}>
+        <h2 className={styles.sectionTitle}>소개</h2>
+        <p className={styles.description}>{movie.description}</p>
+        <span className={styles.readMore}>더보기</span>
+      </section>
+      <div className={styles.reviewSections}>
+        <section>
+          <h2 className={styles.sectionTitle}>내 리뷰 작성하기</h2>
+        </section>
+        <section>
+          <h2 className={styles.sectionTitle}>리뷰</h2>
+          <MovieReviewList movieReviews={movieReviews} />
+        </section>
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,23 @@ import MovieList from '@/components/MovieList';
 import SearchForm from '@/components/SearchForm';
 import styles from '@/styles/Home.module.css';
 import axios from '@/lib/axios';
-import Head from 'next/head';
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-
-  async function getMovies() {
+//정적 생성을 했을때 프롭으로 내려줄 값을 만드는 함수
+export async function getStaticProps() {
     const res = await axios.get('/movies/');
-    const movies = res.data.results ?? [];
-    setMovies(movies);
+    const movies = res.data.results ?? []
+    return{
+      props: {
+        movies
+      },
   }
+}
 
-  useEffect(() => {
-    getMovies();
-  }, []);
+
+export default function Home({movies}) {
 
   return (
     <>
-      <Head>
-        <title>WATCHIT</title>
-      </Head>
         <SearchForm />
         <MovieList className={styles.movieList} movies={movies} />
     </>
